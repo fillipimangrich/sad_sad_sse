ef033485b320e843cf5349077e9576854d38d17d
fillipimangrich <costa.mangrich@gmail.com>
2023-05-08 20:36:21 -0300

    a

---
+++ buffer_gen.py
@@ -0,0 +1,98 @@
+from math import log2, ceil
+
+N = 8
+
+row_counter_size = int(ceil(log2(N)))
+
+
+pre_text = """library ieee;
+
+use ieee.std_logic_1164.all;
+
+use ieee.numeric_std.all;
+
+​
+
+entity io_buffer is
+
+	port (
+
+	clk, rst, wr_en: in std_logic;
+
+	data_in_orig: in std_logic_vector(7 downto 0);
+
+	data_in_cand: in std_logic_vector(7 downto 0);
+
+"""
+
+
+print(pre_text)
+
+
+for i in range(N):
+
+	print("\trow_out_orig%d: out std_logic_vector(7 downto 0);" % (i));
+
+
+
+for i in range(N-1):
+
+	print("\trow_out_cand%d: out std_logic_vector(7 downto 0);" % (i));
+
+print("\trow_out_cand%d: out std_logic_vector(7 downto 0));" % (N-1));
+
+
+print("end io_buffer;")
+
+
+print("\narchitecture behav of io_buffer is")
+
+print("\ntype buff_type is array (0 to %d) of std_logic_vector(7 downto 0);" % (N-1));
+
+print("\nsignal buff_orig, buff_cand: buff_type;")
+
+print("\nsignal row_counter_size: unsigned(%d downto 0);" % (row_counter_size))
+
+print("begin\n")
+
+
+print("process(clk, rst)")
+
+print("begin")
+
+print("\tif rst = '1' then")
+
+print("\t\tbuff_orig <= (others => (others => '0'));")
+
+print("\t\tbuff_cand <= (others => (others => '0'));")
+
+print("\telsif clk'event and clk = '1' then")
+
+print("\t\tif wr_en = '1' then -- write")
+
+print("\t\t\tbuff_orig(to_integer(row_counter_size)) <= data_in_orig;")
+
+print("\t\t\tbuff_cand(to_integer(row_counter_size)) <= data_in_cand;")
+
+print("\t\t\trow_counter_size <= row_counter_size + 1;")
+
+print("\t\tend if;")
+
+print("\tend if;")
+
+print("end process;\n")
+
+
+
+for i in range(N-1):
+
+	print("row_out_orig%d <= buff_orig(%d);" % (i, i));
+
+
+for i in range(N-1):
+
+	print("row_out_cand%d <= buff_cand(%d);" % (i, i));
+
+
+
+print("\nend architecture;")
---
+++ top_level_gen.py
@@ -0,0 +1,73 @@
+from math import ceil, log2
+
+N = 8
+mode = 'sad'
+if mode == 'sad':
+	N_bits_out_minus1 = int(ceil(log2(N) + 7))
+elif mode == 'satd':
+	N_bits_out_minus1 = int(ceil(log2(N) + 7))
+else: # 'ssd'
+	N_bits_out_minus1 = int(ceil(log2(N) + 15))
+
+
+pre_text  = """library ieee;
+use ieee.std_logic_1164.all;
+
+entity top_level_%s is
+	port(clk,rst: in std_logic;
+		 op_en : in std_logic;
+		 wr_en: in std_logic;
+		 data_in_orig: in std_logic_vector(7 downto 0);
+		 data_in_cand: in std_logic_vector(7 downto 0);
+		 data_out: out std_logic_vector(%d downto 0)
+		);
+
+end top_level_%s;
+""" % (mode, N_bits_out_minus1, mode)
+
+print(pre_text)
+print(f"architecture rtl of top_level_{mode} is")
+for i in range(N):
+	print("signal row_out_orig%d: std_logic_vector(7 downto 0); " % (i))
+for i in range(N):
+	print("signal row_out_cand%d: std_logic_vector(7 downto 0); " % (i))
+
+print("signal row_out_orig: std_logic_vector(%d downto 0); " % (N*8-1))
+print("signal row_out_cand: std_logic_vector(%d downto 0); " % (N*8-1))
+
+
+print("""
+begin
+""")
+
+print(f'row_out_orig<= ', end='')
+for i in range(N-1):
+	print("row_out_orig%d & " % (i), end = "")
+print("row_out_orig%d;" % (N-1))
+
+print(f'row_out_cand<= ', end='')
+
+for i in range(N-1):
+	print("row_out_cand%d & " % (i), end = "")
+print("row_out_cand%d;\n" % (N-1))
+
+print("""U_BUFF: entity work.io_buffer port map(clk, rst, wr_er, data_in_orig, data_in_cand,""")
+
+print("\t\t\t\t", end="")
+for i in range(N):
+	print("row_out_orig%d, " % (i), end = "")
+print("\n\t\t\t\t", end="")
+for i in range(N-1):
+	print("row_out_cand%d, " % (i), end = "")
+print("row_out_cand%d);" % (N-1))
+
+print("""	U_PU: entity work.%s%d port map(clk, op_en, rst,
+				row_out_orig,
+				row_out_cand,
+				data_out);""" % (mode,N))
+
+print("\t\t\t\t", end="")
+
+
+
+print("\nend architecture;")
